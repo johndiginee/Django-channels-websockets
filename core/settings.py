ac659bb14616ef59d3236eb61a6f843e6656e492
John Diginee <Johndbizz@gmail.com>
2023-09-20 10:05:09 +0100

    Chore: Configure channels

--- core/settings.py
+++ core/settings.py
@@ -31,14 +31,21 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'livechat'
+
+    'livechat',
 ]
+
+# Channels Config
+
+ASGI_APPLICATION = 'core.asgi.application'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
