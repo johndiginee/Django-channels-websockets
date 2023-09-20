6841017b11e0260d12a43e1a73969ff7e319ee12
John Diginee <Johndbizz@gmail.com>
2023-09-20 10:31:26 +0100

    Feat: Routing configuration in ASGI

--- core/asgi.py
+++ core/asgi.py
@@ -10,10 +10,17 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import livechat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = ProtocolTypeRouter({
     'http':get_asgi_application(),
+    'websocket':AuthMiddlewareStack(
+        URLRouter(
+            livechat.routing.websocket_urlpatterns
+        )
+    )
 })
