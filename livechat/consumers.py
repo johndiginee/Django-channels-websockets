8b314ffc22288748fab7d1d308b8ea7dbc79cb16
John Diginee <Johndbizz@gmail.com>
2023-09-20 11:37:35 +0100

    Feat: Add sending messges

--- livechat/consumers.py
+++ livechat/consumers.py
@@ -9,3 +9,14 @@ class ChatConsumer(WebsocketConsumer):
             'type': 'connection_established',
             'message': 'You are now connected!'
         }))
+    
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        print('Messsage:', message)
+
+        self.send(text_data=json.dumps({
+            'type':'livechat',
+            'message':message
+        }))
